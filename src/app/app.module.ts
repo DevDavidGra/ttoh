fc2edbf949b0d7284a0d27b09831d1935aaf65e5
김준영 <wishcome82@naver.com>
2018-04-07 14:29:46 +0900

    3장

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HerosComponent } from './heros/heros.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Hero} from "../hero";
 
 @Component({
   selector: 'app-heros',
@@ -6,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heros.component.css']
 })
 export class HerosComponent implements OnInit {
+  hero: Hero = {
+    id: 1
+    ,
+    name: 'Windstorm'
 
-  constructor() { }
+  };
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
